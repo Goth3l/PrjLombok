dccb83738e5ec0301fc747df258f80c0cad10660
Goth3l <nycolle.goth3l.dev2024@gmail.com>
2024-11-18 15:43:19 -0300

    Add files via upload

---
+++ PrjGerenciamentoUsuarios/src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.entities;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String name;
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String password;
+}
